3cae2407b99de1ab60ee3d3a9a81c71b6a08589b
jerryshew <jerryshew@gmail.com>
2016-01-31 16:13:17 +0800

    fix express static

--- server.js
+++ server.js
@@ -2,8 +2,9 @@ var webpack = require('webpack')
 var webpackDevMiddleware = require('webpack-dev-middleware')
 var webpackHotMiddleware = require('webpack-hot-middleware')
 var config = require('./webpack.config')
+var express = require('express')
 
-var app = new (require('express'))()
+var app = new (express)()
 var port = 3000
 
 var compiler = webpack(config)
