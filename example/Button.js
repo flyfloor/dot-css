33489a614933619c09f86b69bface3df9377c1f8
jerryshew <jerryshew@gmail.com>
2016-04-17 17:35:38 +0800

    btn

--- example/Button.js
+++ example/Button.js
@@ -1,14 +1,47 @@
 import React, { Component } from 'react';
+import {NS} from './setting';
 
 export default class Button extends Component {
     render() {
         return (
-            <ul>
-                <li>
-                    <p>A standard button</p>
-                    <a className="ui button" href="javascript:;">button</a>
-                </li>
-            </ul>
+            <div>
+                <h2>
+                    Button
+                </h2>
+                <ul>
+                    <li>
+                        <h3>A standard button</h3>
+                        <button className={`${NS} button`}>button</button>
+                    </li>
+                    <li>
+                        <h3>Button size</h3>
+                        <button className={`${NS} tiny button`}>tiny</button>
+                        <button className={`${NS} small button`}>small</button>
+                        <button className={`${NS} button`}>normal</button>
+                        <button className={`${NS} large button`}>large</button>
+                        <button className={`${NS} huge button`}>huge</button>
+                    </li>
+                    <li>
+                        <h3>Colored button</h3>
+                        <button className={`${NS} red button`}>red</button>
+                        <button className={`${NS} pink button`}>pink</button>
+                        <button className={`${NS} purple button`}>purple</button>
+                        <button className={`${NS} indigo button`}>indigo</button>
+                        <button className={`${NS} cyan button`}>cyan</button>
+                        <button className={`${NS} teal button`}>teal</button>
+                        <button className={`${NS} green button`}>green</button>
+                        <button className={`${NS} lime button`}>lime</button>
+                        <button className={`${NS} amber button`}>amber</button>
+                        <button className={`${NS} orange button`}>orange</button>
+                        <button className={`${NS} brown button`}>brown</button>
+                        <button className={`${NS} gray button`}>gray</button>
+                        <button className={`${NS} bluegray button`}>bluegray</button>
+                        <button className={`${NS} yellow button`}>yellow</button>
+                        <button className={`${NS} black button`}>black</button>
+                        <button className={`${NS} white button`}>white</button>
+                    </li>
+                </ul>
+            </div>
         );
     }
 }
