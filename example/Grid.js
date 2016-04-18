c6e34b54f7b2bb181d4ceb96d0ec7ecb4fa4c221
jerryshew <jerryshew@gmail.com>
2016-04-18 16:48:16 +0800

    add row to grid

--- example/Grid.js
+++ example/Grid.js
@@ -9,60 +9,49 @@ export default class Grid extends Component {
                 <ul>
                     <li>
                         <h4>same width column</h4>
-                        <ul className={`${NS} grid grid-5 grid-example text-center text-extra`}>
-                            <li className="column">
-                                <p>column1
-                                </p>
-                            </li>
-                            <li className="column">
-                                <p>column2
-                                </p>
-                            </li>
-                            <li className="column">
-                                <p>column3
-                                </p>
-                            </li>
-                            <li className="column">
-                                <p>column4
-                                </p>
-                            </li>
-                            <li className="column">
-                                <p>column5
-                                </p>
-                            </li>
-                        </ul>
+                        <div className={`${NS} grid grid-5 grid-example text-center text-extra`}>
+                            <div className="row">
+                                <div className="column">
+                                    <p>column1</p>
+                                </div>
+                                <div className="column">
+                                    <p>column2</p>
+                                </div>
+                                <div className="column">
+                                    <p>column3</p>
+                                </div>
+                                <div className="column">
+                                    <p>column4</p>
+                                </div>
+                                <div className="column">
+                                    <p>column5</p>
+                                </div>
+                            </div>
+                        </div>
                         <ul className={`${NS} grid grid-8 grid-example text-center`}>
                             <li className="column">
-                                <p>column1
-                                </p>
+                                <p>column1</p>
                             </li>
                             <li className="column">
-                                <p>column2
-                                </p>
+                                <p>column2</p>
                             </li>
                             <li className="column">
-                                <p>column3
-                                </p>
+                                <p>column3</p>
                             </li>
                             <li className="column">
-                                <p>column4
-                                </p>
+                                <p>column4</p>
                             </li>
                             <li className="column">
-                                <p>column5
-                                </p>
+                                <p>column5</p>
                             </li>
                             <li className="column">
-                                <p>column6
-                                </p>
+                                <p>column6</p>
                             </li>
                             <li className="column">
-                                <p>column7
-                                </p>
+                                <p>column7</p>
                             </li>
                             <li className="column">
-                                <p>column8
-                                </p>
+                                <p>column8</p>
                             </li>
                         </ul>
                         <ul className={`${NS} grid grid-4 grid-example text-center`}>
@@ -87,37 +76,49 @@ export default class Grid extends Component {
                     </li>
                     <li>
                         <h3>column width, divide to 16 part</h3>
-                        <ul className={`${NS} grid grid-example text-center`}>
-                            <li className="column column-2">
-                                <p>2 part column</p>
-                            </li>
-                            <li className="column column-6">
-                                <p>6 part column</p>
-                            </li>
-                            <li className="column column-4">
-                                <p>4 part column</p>
-                            </li>
-                            <li className="column column-2">
-                                <p>2 part column</p>
-                            </li>
-                            <li className="column column-2">
-                                <p>2 part column</p>
-                            </li>
-                        </ul>
+                        <div className={`${NS} grid grid-example text-center`}>
+                            <div className="row">
+                                <div className="column column-2">
+                                    <p>2 part column</p>
+                                </div>
+                                <div className="column column-6">
+                                    <p>6 part column</p>
+                                </div>
+                                <div className="column column-2">
+                                    <p>2 part column</p>
+                                </div>
+                                <div className="column column-2">
+                                    <p>2 part column</p>
+                                </div>
+                                <div className="column column-4">
+                                    <p>4 part column</p>
+                                </div>
+                            </div>
+                            <div className="row">
+                                <div className="column column-8">
+                                    <p>8 part column</p>
+                                </div>
+                                <div className="column column-8">
+                                    <p>8 part column</p>
+                                </div>
+                            </div>
+                        </div>
                     </li>
                     <li>
                         <h3>column width, divide to 16 part</h3>
-                        <ul className={`${NS} grid grid-example text-center`}>
-                            <li className="column column-3">
-                                <p>3 part column</p>
-                            </li>
-                            <li className="column column-5">
-                                <p>5 part column</p>
-                            </li>
-                            <li className={`${NS} float-right column column-4`}>
-                                <p>4 part column</p>
-                            </li>
-                        </ul>
+                        <div className={`${NS} grid grid-example text-center`}>
+                            <div className="row">
+                                <div className="column column-3">
+                                    <p>3 part column</p>
+                                </div>
+                                <div className="column column-5">
+                                    <p>5 part column</p>
+                                </div>
+                                <div className={`${NS} float-right column column-4`}>
+                                    <p>4 part column</p>
+                                </div>
+                            </div>
+                        </div>
                     </li>
                 </ul>
             </div>
