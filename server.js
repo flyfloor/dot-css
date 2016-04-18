e65007963c02f8bcb13c3ee9fc2860443c71f4b2
jerryshew <jerryshew@gmail.com>
2016-04-18 21:41:35 +0800

    add port config

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var config = require('./webpack.config');
 var express = require('express');
 
 var app = new (express)();
-var port = 3000;
+var port = process.env.PORT || (process.argv[2] || 3000);
 
 var compiler = webpack(config);
 app.use(webpackDevMiddleware(compiler, { noInfo: true, publicPath: config.output.publicPath }));
