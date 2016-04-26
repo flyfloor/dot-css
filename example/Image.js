403ef14ab2185b5f71e00da38dc992cf433f0025
jerryshew <jerryshew@gmail.com>
2016-04-27 01:20:13 +0800

    image circle

--- example/Image.js
+++ example/Image.js
@@ -21,6 +21,9 @@ export default class Image extends Component {
                         <div className={CN('small image')}>
                             <img src="http://braavos.me/dot-css/dist/img/img.png"/>
                         </div>
+                        <div className={CN('middle image')}>
+                            <img src="http://braavos.me/dot-css/dist/img/img.png"/>
+                        </div>
                         <div className={CN('large image')}>
                             <img src="http://braavos.me/dot-css/dist/img/img.png"/>
                         </div>
@@ -30,8 +33,25 @@ export default class Image extends Component {
                         <div className={CN('huge image')}>
                             <img src="http://braavos.me/dot-css/dist/img/img.png"/>
                         </div>
+                        <h4>Round image</h4>
+                        <div className={CN('round image')}>
+                            <img src="http://braavos.me/dot-css/dist/img/img.png"/>
+                        </div>
                     </li>
                     <li>
+                        <h3>Circle image</h3>
+                        <h4>Default circle image</h4>
+                        <div className={CN('circle tiny image')}>
+                            <img src="http://braavos.me/dot-css/dist/img/avatar.png"/>
+                        </div>
+                        <h4>Landscape circle image</h4>
+                        <div className={CN('circle small landscape image')}>
+                            <img src="http://braavos.me/dot-css/dist/img/img.png"/>
+                        </div>
+                        <h4>Portrait circle image</h4>
+                        <div className={CN('circle small portrait image')}>
+                            <img src="http://braavos.me/dot-css/dist/img/img0.png"/>
+                        </div>
                     </li>
                     <li></li>
                 </ul>
