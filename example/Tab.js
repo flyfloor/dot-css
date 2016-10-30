4d23e6f0d72472c977756086e46d608327644465
jerryshew <jerryshew@gmail.com>
2016-10-30 18:24:38 +0800

    bordered

--- example/Tab.js
+++ example/Tab.js
@@ -11,52 +11,111 @@ export default class Tab extends Component {
                     <li>
                         <h4>Default Tab</h4>
                         <div className={CN('tab')}>
-                            <div className="item active">tab 1</div>
-                            <div className="item">tab 2</div>
-                            <div className="item">tab 3</div>
+                            <div className="bar">
+                                <div className="item active">tab 1</div>
+                                <div className="item">tab 2</div>
+                                <div className="item">tab 3</div>
+                            </div>
+                            <div className="content">
+                                <h3>This is content</h3>
+                                <p>test</p>
+                                <br/>
+                                <br/>
+                                <br/>
+                                <br/>
+                            </div>
                         </div>
-                        <div className="dot module">
-                            <h3>This is content</h3>
-                            <p>test</p>
-                            <br/>
-                            <br/>
-                            <br/>
-                            <br/>
+                    </li>
+                    <li>
+                        <h4>Bottom Tab</h4>
+                        <div className={CN('bottom tab')}>
+                            <div className="content">
+                                <h3>This is content</h3>
+                                <p>test</p>
+                                <br/>
+                                <br/>
+                                <br/>
+                                <br/>
+                            </div>
+                            <div className="bar">
+                                <div className="item active">tab 1</div>
+                                <div className="item">tab 2</div>
+                                <div className="item">tab 3</div>
+                            </div>
                         </div>
                     </li>
                     <li>
                         <h4>Section tab</h4>
                         <div className={CN('section tab')}>
-                            <div className="item active">tab 1</div>
-                            <div className="item">tab 2</div>
-                            <div className="item">tab 3</div>
+                            <div className="bar">
+                                <div className="item active">tab 1</div>
+                                <div className="item">tab 2</div>
+                                <div className="item">tab 3</div>
+                            </div>
+                            <div className="content">
+                                <h3>This is content</h3>
+                                <p>test...</p>
+                                <br/>
+                                <br/>
+                                <br/>
+                                <br/>
+                            </div>
                         </div>
-                        <div className="dot module">
-                            <h3>This is content</h3>
-                            <p>test...</p>
-                            <br/>
-                            <br/>
-                            <br/>
-                            <br/>
+                    </li>
+                    <li>
+                        <h4>Bottom Section tab</h4>
+                        <div className={CN('bottom section tab')}>
+                            <div className="content">
+                                <h3>This is content</h3>
+                                <p>test...</p>
+                                <br/>
+                                <br/>
+                                <br/>
+                                <br/>
+                            </div>
+                            <div className="bar">
+                                <div className="item active">tab 1</div>
+                                <div className="item">tab 2</div>
+                                <div className="item">tab 3</div>
+                            </div>
                         </div>
                     </li>
                     <li>
                         <h4>Vertical tab</h4>
                         <div className={CN('vertical tab')}>
-                            <div className="item active">tab 1</div>
-                            <div className="item">tab 2</div>
-                            <div className="item">tab 3</div>
-                        </div>
-                        <div className="dot module">
-                            <h3>This is content</h3>
-                            <p>test...</p>
-                            <br/>
-                            <br/>
-                            <br/>
-                            <br/>
+                            <div className="bar">
+                                <div className="item active">tab 1</div>
+                                <div className="item">tab 2</div>
+                                <div className="item">tab 3</div>
+                            </div>
+                            <div className="content">
+                                <h3>This is content</h3>
+                                <p>test...</p>
+                                <br/>
+                                <br/>
+                                <br/>
+                                <br/>
+                            </div>
                         </div>
                     </li>
-                    <li></li>
+                    <li>
+                        <h4>Right Vertical tab</h4>
+                        <div className={CN('right vertical tab')}>
+                            <div className="content">
+                                <h3>This is content</h3>
+                                <p>test...</p>
+                                <br/>
+                                <br/>
+                                <br/>
+                                <br/>
+                            </div>
+                            <div className="bar">
+                                <div className="item active">tab 1</div>
+                                <div className="item">tab 2</div>
+                                <div className="item">tab 3</div>
+                            </div>
+                        </div>
+                    </li>
                 </ul>
             </div>
         );
